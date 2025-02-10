595246c57e4151bdfe7f14c50ac6f83880e1ecc3
ragul0427 <ragulhp27@gmail.com>
2025-02-10 14:56:16 +0530

    first commit-6

---
+++ packages/email-builder/src/index.d.ts
@@ -0,0 +1,3 @@
+export { default as renderToStaticMarkup } from './renderers/renderToStaticMarkup';
+export { ReaderBlockSchema, TReaderBlock, ReaderDocumentSchema, TReaderDocument, ReaderBlock, TReaderBlockProps, TReaderProps, default as Reader, } from './Reader/core';
+//# sourceMappingURL=index.d.ts.map
---
+++ packages/email-builder/src/index.js
@@ -0,0 +1,7 @@
+export { default as renderToStaticMarkup } from './renderers/renderToStaticMarkup';
+export { ReaderBlockSchema, 
+//
+ReaderDocumentSchema, 
+//
+ReaderBlock, default as Reader, } from './Reader/core';
+//# sourceMappingURL=index.js.map
